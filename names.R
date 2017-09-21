e6757617d1c58d97614206e42b72079cecef2197
unknown <stmillett@gmail.com>
2017-09-20 19:47:13 -0700

    message

---
+++ names.R
@@ -0,0 +1,17 @@
+# Names.R
+# Created by [GROUP NAME HERE]
+# Date: [INSERT DATE HERE]
+# Tell us what your name is!
+
+# Original Repo-Owner, please write in here
+print("Hello, my name is [name here]")
+
+# Collaborators: Repeat this above command with your name.
+# Don't do this *for* other people.  Just let them pull the repo, add a line for their own name, commit, and push it!
+# Remember not to do this all at once.  Have one group member do it at a time to prevent conflicts.
+
+print("")
+
+print("")
+
+print("")
